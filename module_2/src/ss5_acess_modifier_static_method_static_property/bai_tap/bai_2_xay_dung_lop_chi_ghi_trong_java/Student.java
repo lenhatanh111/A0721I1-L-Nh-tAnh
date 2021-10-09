0c28e90a5539003d5c0808499a5cb06855cf78fe
le nhat anh <nhatanh28012000@gmail.com>
2021-10-09 17:04:33 +0700

    up

---
+++ module_2/src/ss5_acess_modifier_static_method_static_property/bai_tap/bai_2_xay_dung_lop_chi_ghi_trong_java/Student.java
@@ -0,0 +1,17 @@
+package ss5_acess_modifier_static_method_static_property.bai_tap.bai_2_xay_dung_lop_chi_ghi_trong_java;
+
+public class Student {
+    private String name="john";
+    private String classes="C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ module_2/src/ss5_acess_modifier_static_method_static_property/bai_tap/bai_2_xay_dung_lop_chi_ghi_trong_java/Test.java
@@ -0,0 +1,9 @@
+package ss5_acess_modifier_static_method_static_property.bai_tap.bai_2_xay_dung_lop_chi_ghi_trong_java;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student=new Student();
+        student.setClasses("A0721I1");
+        student.setName("lê nhật anh");
+    }
+}
