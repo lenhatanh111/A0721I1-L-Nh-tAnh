c8e949475f9665df549d21694d1ee1ce91d7aaf7
le nhat anh <nhatanh28012000@gmail.com>
2021-12-08 20:07:44 +0700

    up

--- module_3/bai_2_thiet_ke_va_tao_co_so_du_lieu/bai_tap/bai_2_xay_dung_csdl_quan_ly_ban_hang.sql
+++ module_3/bai_2_thiet_ke_va_tao_co_so_du_lieu/bai_tap/bai_2_xay_dung_csdl_quan_ly_ban_hang.sql
@@ -1,22 +1,22 @@
 create database sales_manage;
 use sales_manage;
 create table customer(
-c_id int primary key,
+c_id int primary key auto_increment,
 c_name varchar(30),
 c_age int
 );
 create table `order`(
-o_id int primary key,
+o_id int primary key auto_increment,
 c_id int,
 foreign key(c_id) references customer(c_id),
 o_date date,
-o_total_price double
+o_total_price int
 
 );
 create table product(
-p_id int primary key,
+p_id int primary key auto_increment,
 p_name varchar(50),
-p_price double
+p_price int
 );
 create table order_detal(
 o_id int,
---
+++ module_3/bai_3_thao_tac_voi_csdl/bai_tap/bai_2_thao_tac_voi_csdl_quan_ly_ban_hang.sql
@@ -0,0 +1,12 @@
+use sales_manage;
+insert into customer(c_name,c_age) values ('Minh quan','10'),('Ngoc Oanh','20'),('Hong Ha','50');
+insert into `order`(c_id,o_date) values('1','2006/3/21'),('2','2006/3/23'),('1','2006/3/16');
+insert into product(p_name,p_price) values('May Giat','3'),('Tu lanh','5'),('Dieu Hoa','7'),('Quat','1'),('Bep Dien','2');
+insert into order_detal values('1','1','3'),('1','3','7'),('1','4','2'),('2','1','1'),('3','1','8'),
+('2','5','4'),('2','3','3');
+
+select `order`.o_id,`order`.o_date,product.p_price from `order`left join `order_detal` on `order`.o_id=order_detal.o_id
+left join product on order_detal.p_id=product.p_id;
+select customer.c_name,product.p_name from `order` left join customer on customer.c_id=`order`.c_id
+left join `order_detal` on `order`.o_id=order_detal.o_id
+left join product on order_detal.p_id=product.p_id;
