0d1bab06da7b88dd34d53549f9e1b410b00aa5cc
Lê Nhật Anh <nhatanh28012000@gmail.com>
2022-01-05 19:34:37 +0700

    up

---
+++ module_3/bai_9_tong_quan_ung_dung_web/thuc_hanh/src/main/java/ss9_tong_quan_ung_dung_web/thuc_hanh/bai_2_tao_trang_web_de_dang_nhap_va_hien_thi_loi_chao/LoginServlet.java
@@ -0,0 +1,32 @@
+package ss9_tong_quan_ung_dung_web.thuc_hanh.bai_2_tao_trang_web_de_dang_nhap_va_hien_thi_loi_chao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
