44f786ba0880259508a4ae4d56c710be6f69ab80
Lê Nhật Anh <nhatanh28012000@gmail.com>
2022-01-21 19:00:08 +0700

    up

---
+++ module_3/Module2_AccountName_WBE/src/main/java/com/codegym/model/customer.java
@@ -0,0 +1,11 @@
+package com.codegym.model;
+
+import java.util.Date;
+
+public class customer {
+    protected int customer_id;
+    protected String customer_name;
+    protected Date customer_birthday;
+    protected int customer_gender;
+    protected int customer_gender;
+}
