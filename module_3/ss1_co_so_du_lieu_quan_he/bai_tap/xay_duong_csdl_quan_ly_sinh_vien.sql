00a148d5f28d13854a7b32426949df01c312e7ff
Lê Nhật Anh <nhatanh28012000@gmail.com>
2022-02-18 20:02:30 +0700

    up

---
+++ module_3/ss1_co_so_du_lieu_quan_he/bai_tap/xay_duong_csdl_quan_ly_sinh_vien.sql
@@ -0,0 +1,11 @@
+use `bai1_csdl_quan_he`;
+create table class(
+id int not null,
+`name` varchar(45)
+);
+create table teacher(
+id int not null,
+`name` varchar(45),
+age int,
+country varchar(50)
+);
---
+++ module_3/ss1_co_so_du_lieu_quan_he/thuc_hanh/bai_3_tao_bang_tren_mysql_workbench/bai_3_tao_bang_tren_workbench.sql
@@ -0,0 +1,10 @@
+create database `bai1_csdl_quan_he`;
+use `bai1_csdl_quan_he`;
+create table `student_management`(
+id int not null,
+`name` varchar(45) null,
+age int,
+country varchar(45) null,
+primary key (id)
+
+);
